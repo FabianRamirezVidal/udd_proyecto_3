dba1574ceef9db52befcaee2e33244f10f432fb8
Fabian Ramirez <framirezvidal@gmail.com>
2023-03-29 22:30:01 -0300

    Agregar_clima_por_locación

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,39 @@
-import { createChart } from './charts.js';
-import { getCoronavirusData } from './coronavirus-api.js';
+let lon;
+let lat;
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let location = document.querySelector(".location");
+let icon = document.querySelector(".icon");
+const kelvin = 273.15;
 
-// const labels = [ 'Enero', 'Febrero', 'Marzo', 'Abril' ]
+window.addEventListener("load",()=> {
+  if(navigator.geolocation)
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    lon = position.coords.longitude;
+    lat = position.coords.latitude;
 
-// const datasets = [ {
-//   label: 'Cantidad de errores por mes',
-//   backgroundColor: 'rgb(255, 99, 132)',
-//   borderColor: 'rgb(255, 99, 132)',
-//   data: [ 40, 66, 33, 98 ]
-// } ]
+    const api_id = "9025ae5819d54f95517bd664b8f5901a"
+    const url_base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&`+`lon=${lon}&appid=${api_id}`;
 
-const data = await getCoronavirusData( 'chile', 'deaths' )
-console.log( `🚀 ~ data:`, data );
+    fetch(url_base)
+    .then((response) => {
 
-const from = 2 // Enero
-const to = 2 // Enero
-const currentYear = 2023
+      return response.json();
 
-const filteredData = data.filter( dataPerDay => {
-  const date = new Date( dataPerDay.Date )
-  const month = date.getMonth()
-  const year = date.getFullYear()
-  return from <= month && month <= to && currentYear == year
-} )
+    })
+    .then((data) => {
+console.log("esta es la data")
+console.log(data);
 
-const labels = filteredData.map( dataPerDay => new Date( dataPerDay.Date ).toLocaleDateString() )
+temperature.textContent = Math.floor(data.main.temp - kelvin) + "ºC"
+summary.textContent = data.weather[0].description;
+location.textContent = data.name + ","+ data.sys.country;
+let icon_image = `https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png`;
+icon.innerHTML = "<img width=\"40px\" height=\"40px\">";
+icon.setAttribute ("src",icon_image) 
 
-const datasets = [ {
-  label: 'Cantidad de casos confirmados',
-  backgroundColor: 'rgb(255, 99, 132)',
-  borderColor: 'rgb(255, 99, 132)',
-  data: filteredData.map( dataPerDay => dataPerDay.Cases )
-} ]
+    });
+  });
 
-const canvasNode = document.getElementById( 'myChart' )
-createChart( labels, datasets, canvasNode, 'line' )
+});
