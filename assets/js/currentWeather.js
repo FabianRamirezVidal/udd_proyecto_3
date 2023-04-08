bb2cbade794ab3aa261734753e86002317ac79fd
Fabian Ramirez <framirezvidal@gmail.com>
2023-04-07 21:33:01 -0400

    async await

--- assets/js/currentWeather.js
+++ assets/js/currentWeather.js
@@ -9,36 +9,35 @@ let icon = document.querySelector(".icon");
 const kelvin = 273.15;
 const api_id = "9025ae5819d54f95517bd664b8f5901a";
 
-function getCurrentWeather() {
-    return new Promise((resolve, reject) => {
+async function getCurrentWeather() {
+    const position = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition((position) => {
-            const lon = position.coords.longitude;
-            const lat = position.coords.latitude;
-            const url_base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=${api_id}`;
-
-            fetch(url_base)
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    console.log(data);
-                    humidity.textContent = "Humidity: " + Math.floor(data.main.humidity) + "%"
-                    temperature.textContent = "Temperature: " + Math.floor(data.main.temp - kelvin) + "ºC"
-                    tempSense.textContent = "Feels Like: " + Math.floor(data.main.temp - kelvin) + "ºC"
-                    maxTemp.textContent = "Max Temperature: " + Math.floor(data.main.temp_max - kelvin) + "ºC"
-                    minTemp.textContent = "Min Temperature: " + Math.floor(data.main.temp_min - kelvin) + "ºC"
-                    summary.textContent = "Condition: " + data.weather[0].description;
-                    location.textContent = "Location: " + data.name + "," + data.sys.country;
-                    let icon_image = `https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png`;
-                    icon.innerHTML = "<img width=\"40px\" height=\"40px\">";
-                    icon.setAttribute("src", icon_image);
-                    resolve();
-                })
-                .catch((error) => {
-                    reject(error);
-                });
+            resolve(position);
+        }, (error) => {
+            reject(error);
         });
     });
+    const lon = position.coords.longitude;
+    const lat = position.coords.latitude;
+    const url_base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=${api_id}`;
+
+    try {
+        const response = await fetch(url_base);
+        const data = await response.json();
+        console.log(data);
+        humidity.textContent = "Humidity: " + Math.floor(data.main.humidity) + "%"
+        temperature.textContent = "Temperature: " + Math.floor(data.main.temp - kelvin) + "ºC"
+        tempSense.textContent = "Feels Like: " + Math.floor(data.main.temp - kelvin) + "ºC"
+        maxTemp.textContent = "Max Temperature: " + Math.floor(data.main.temp_max - kelvin) + "ºC"
+        minTemp.textContent = "Min Temperature: " + Math.floor(data.main.temp_min - kelvin) + "ºC"
+        summary.textContent = "Condition: " + data.weather[0].description;
+        location.textContent = "Location: " + data.name + "," + data.sys.country;
+        let icon_image = `https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png`;
+        icon.innerHTML = "<img width=\"40px\" height=\"40px\">";
+        icon.setAttribute("src", icon_image);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export { getCurrentWeather };
--- assets/js/weatherForecast.js
+++ assets/js/weatherForecast.js
@@ -1,81 +1,77 @@
 const kelvin = 273.15;
 const api_id = "9025ae5819d54f95517bd664b8f5901a";
 
-function getWeatherForecast() {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const lon = position.coords.longitude;
-            const lat = position.coords.latitude;
-            const url_fore = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&` + `lon=${lon}&appid=${api_id}`;
-
-            fetch(url_fore)
-                .then((response) => {
-                    return response.json();
-                })
-                .then((dataFore) => {
-                    const data1 = dataFore.list;
-                    console.log(data1)
-                    let labels = data1.map(function (element) {
-                        return element.dt_txt;
-                    });
-                    let temps = data1.map(function (element) {
-                        return Math.floor(element.main.temp - kelvin);
-                    });
-                    let hums = data1.map(function (element) {
-                        return Math.floor(element.main.humidity);
-                    });
-
-                    const ctx = document.getElementById('myChart');
-
-                    new Chart(ctx, {
-                        type: 'line',
-                        mantainAspectRatio: false,
-                        responsive: true,
-                        resizeDelay: 0,
-                        data: {
-                            labels: labels,
-                            datasets: [{
-                                label: 'Temperatura en ºC',
-                                data: temps,
-                                yAxisID:"y",
-                                borderWidth: 1},{
-                                label: 'Humedad en %',
-                                data: hums,
-                                yAxisID:"percentage"
-                            }]
-                        },
-                        options: {
-                            scales: {
-                                y: {
-                                    beginAtZero: true,
-                                    type: "linear",
-                                    position: "left",
-                                    ticks: {
-                                        callback: function(value, index, values) {
-                                            return `${value} ºC`
-                                        },
-                                    }
-                                },
-                                percentage: {
-                                    beginAtZero: false,
-                                    type: "linear",
-                                    position: "right",
-                                    ticks: {
-                                        callback: function(value, index, values) {
-                                            return `${value} %`
-                                        },
-                                    }
-                                }
-                            }
-                        }
-                    });
-                    resolve();
-                })
-                .catch((error) => {
-                    reject(error);
-                });
+async function getWeatherForecast() {
+    try {
+        const position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
         });
-    });
+        const lon = position.coords.longitude;
+        const lat = position.coords.latitude;
+        const url_fore = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&` + `lon=${lon}&appid=${api_id}`;
+
+        const response = await fetch(url_fore);
+        const dataFore = await response.json();
+
+        const data1 = dataFore.list;
+        console.log(data1)
+        let labels = data1.map(function (element) {
+            return element.dt_txt;
+        });
+        let temps = data1.map(function (element) {
+            return Math.floor(element.main.temp - kelvin);
+        });
+        let hums = data1.map(function (element) {
+            return Math.floor(element.main.humidity);
+        });
+
+        const ctx = document.getElementById('myChart');
+
+        new Chart(ctx, {
+            type: 'line',
+            mantainAspectRatio: false,
+            responsive: true,
+            resizeDelay: 0,
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Temperatura en ºC',
+                    data: temps,
+                    yAxisID:"y",
+                    borderWidth: 1},{
+                    label: 'Humedad en %',
+                    data: hums,
+                    yAxisID:"percentage"
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        type: "linear",
+                        position: "left",
+                        ticks: {
+                            callback: function(value, index, values) {
+                                return `${value} ºC`
+                            },
+                        }
+                    },
+                    percentage: {
+                        beginAtZero: false,
+                        type: "linear",
+                        position: "right",
+                        ticks: {
+                            callback: function(value, index, values) {
+                                return `${value} %`
+                            },
+                        }
+                    }
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export { getWeatherForecast };
